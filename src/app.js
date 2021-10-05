c94ca07bed66236ea9d5e1cf1702f2521aa5d5ca
Şükrü Uzel <sukru.uzel@gmail.com>
2021-10-05 10:46:45 +0300

    Update config files

--- src/app.js
+++ src/app.js
@@ -12,54 +12,52 @@ app.use(express.json())
 
 const binance = new Binance().options({
   APIKEY: process.env.BINANCE_API_KEY,
-  APISECRET: process.env.BINANCE_API_SECRET
+  APISECRET: process.env.BINANCE_API_SECRET,
 })
 
 const hook = new Webhook(process.env.DISCORD_WEBHOOK_URL)
 
-app.get('/health', async (req, res) => res.json({'status':'UP'}))
+app.get('/health', async (req, res) => res.json({ status: 'UP' }))
 
 app.post('/webhook', async (req, res) => {
-
-  let { exchange, passphrase, quantity = 0, side = 'buy', ticker } = req.body
+  const { exchange, passphrase, quantity = 0, side = 'buy', ticker } = req.body
 
   // Validation
-  if (exchange != 'BINANCE') {
-    res.json({ 'code': 'error', 'message': 'Invalid exchange' })
+  if (exchange !== 'BINANCE') {
+    res.json({ code: 'error', message: 'Invalid exchange' })
     return
   }
 
-  if (passphrase != process.env.WEBHOOK_PASSPHRASE) {
-    res.json({ 'code': 'error', 'message': 'Invalid passphrase' })
+  if (passphrase !== process.env.WEBHOOK_PASSPHRASE) {
+    res.json({ code: 'error', message: 'Invalid passphrase' })
     return
   }
 
   // Buy
-  if (side == 'buy') {
+  if (side === 'buy') {
     try {
       console.info(`Sending order : Market Buy - ${ticker} ${quantity}`)
       await binance.marketBuy(ticker, quantity)
-      res.json({ 'code': 'success', 'message': 'order executed' })
+      res.json({ code: 'success', message: 'order executed' })
     } catch (err) {
       console.error(`Error : ${err}`)
-      res.json({ 'code': 'error', 'message': err.body })
+      res.json({ code: 'error', message: err.body })
     }
   }
 
   // Sell
-  if (side == 'sell') {
+  if (side === 'sell') {
     try {
       console.info(`Sending order : Market Sell - ${ticker} ${quantity}`)
       await binance.marketSell(ticker, quantity)
-      res.json({ 'code': 'success', 'message': 'order executed' })
+      res.json({ code: 'success', message: 'order executed' })
     } catch (err) {
       console.error(`Error : ${err}`)
-      res.json({ 'code': 'error', 'message': err.body })
+      res.json({ code: 'error', message: err.body })
     }
   }
 
   hook.send(`Sending order : Market ${side} - ${ticker} ${quantity}`)
-
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
