63cb209b5b4b0fc03ef6e64d05e6f14b0f888f3c
Şükrü Uzel <sukru.uzel@gmail.com>
2021-10-07 14:15:18 +0300

    Update methods

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const Binance = require('node-binance-api')
 const { Webhook } = require('discord-webhook-node')
+const mins = require('./mins.json')
 
 const app = express()
 const port = 3000
@@ -15,12 +16,25 @@ const binance = new Binance().options({
   APISECRET: process.env.BINANCE_API_SECRET,
 })
 
+binance.balance((error, balances) => {
+  if (error) return console.error(error)
+  app.locals.balances = balances
+  return balances
+})
+
 const hook = new Webhook(process.env.DISCORD_WEBHOOK_URL)
 
 app.get('/health', async (req, res) => res.json({ status: 'UP' }))
 
 app.post('/webhook', async (req, res) => {
-  const { exchange, passphrase, quantity = 0, side = 'buy', ticker } = req.body
+  const {
+    exchange,
+    passphrase,
+    price = 0,
+    quantity = 0,
+    side = 'buy',
+    ticker,
+  } = req.body
 
   // Validation
   if (exchange !== 'BINANCE') {
@@ -38,8 +52,14 @@ app.post('/webhook', async (req, res) => {
   // Buy
   if (side === 'buy') {
     try {
-      console.info(`Sending order : Market Buy - ${ticker} ${quantity}`)
-      await binance.marketBuy(ticker, quantity)
+      let qty = quantity
+      if (price) {
+        const tickers = await binance.bookTickers(ticker)
+        qty = price / tickers.askPrice
+        qty = binance.roundStep(qty, mins[ticker].stepSize)
+      }
+      console.info(`Sending order : Market Buy - ${ticker} ${qty}`)
+      await binance.marketBuy(ticker, qty)
       res.json({ code: 'success', message: 'order executed' })
     } catch (err) {
       console.error(`Error : ${err}`)
@@ -51,8 +71,9 @@ app.post('/webhook', async (req, res) => {
   // Sell
   if (side === 'sell') {
     try {
-      console.info(`Sending order : Market Sell - ${ticker} ${quantity}`)
-      await binance.marketSell(ticker, quantity)
+      const qty = app.locals.balances[ticker.replace('USDT', '')].available
+      console.info(`Sending order : Market Sell - ${ticker} ${qty}`)
+      await binance.marketSell(ticker, qty)
       res.json({ code: 'success', message: 'order executed' })
     } catch (err) {
       console.error(`Error : ${err}`)
@@ -60,6 +81,9 @@ app.post('/webhook', async (req, res) => {
       res.json({ code: 'error', message: err.body })
     }
   }
+
+  // Update Balances
+  app.locals.balances = await binance.balance()
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
