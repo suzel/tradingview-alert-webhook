e1d1a16eee22187968cc9f527d5223d774e6ffb5
Şükrü Uzel <sukru.uzel@gmail.com>
2021-10-14 12:21:43 +0300

    Update method

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
-const axios = require('axios')
+// const axios = require('axios')
 const express = require('express')
 const Binance = require('node-binance-api')
 
-const binanceAPI = axios.create({ baseURL: process.env.BINANCE_API_URL })
+// const binanceAPI = axios.create({ baseURL: process.env.BINANCE_API_URL })
 
 const binance = new Binance().options({
   APIKEY: process.env.BINANCE_API_KEY,
@@ -32,20 +32,11 @@ app.post('/webhook', async (req, res) => {
     return
   }
 
-  const ticker = order.ticker
+  const { ticker } = order
   const action = order.strategy.order_action
   const quantity = order.strategy.position_size
 
   console.info(`Sending order : Market ${action} - ${ticker} ${quantity}`)
-
-  switch (action) {
-    case 'buy':
-      binance.marketBuy(ticker, quantity, resultHandler)
-      break
-    case 'sell':
-      binance.marketSell(ticker, quantity, resultHandler)
-      break
-  }
 
   const resultHandler = (error, resp) => {
     const message = error
@@ -54,18 +45,40 @@ app.post('/webhook', async (req, res) => {
     console.log(message)
     res.json({ code: error ? 'error' : 'success', message })
   }
+
+  switch (action) {
+    case 'buy':
+      binance.marketBuy(ticker, quantity, resultHandler)
+      break
+    case 'sell':
+      binance.marketSell(ticker, quantity, resultHandler)
+      break
+    default:
+      break
+  }
 })
 
-const roundStep = async (ticker, price) => {
+/* const roundStep = async (ticker, price) => {
   const [bookTicker, exchangeInfo] = await Promise.all([
     binanceAPI.get(`/ticker/bookTicker?symbol=${ticker}`),
     binanceAPI.get(`/exchangeInfo?symbol=${ticker}`),
   ])
-  const stepSize = exchangeInfo.data.symbols[0].filters.filter(
+  const { stepSize } = exchangeInfo.data.symbols[0].filters.filter(
     (i) => i.filterType === 'LOT_SIZE'
-  )[0].stepSize
+  )[0]
   const qty = price / bookTicker.data.askPrice
   return binance.roundStep(qty, stepSize)
-}
+} */
+
+// // Set minimum order amount with minQty
+// if ( amount < minQty ) amount = minQty;
+
+// // Set minimum order amount with minNotional
+// if ( price * amount < minNotional ) {
+// 	amount = minNotional / price;
+// }
+
+// // Round to stepSize
+// amount = binance.roundStep(amount, stepSize);
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
